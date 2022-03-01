18c508889d53dc6da0a32bf220c867e5ab2173aa
Garlic <long1390@gmail.com>
2022-03-01 15:10:01 +0800

    fixed

--- packages/firebase_dynamic_links/firebase_dynamic_links_platform_interface/lib/src/pending_dynamic_link_data.dart
+++ packages/firebase_dynamic_links/firebase_dynamic_links_platform_interface/lib/src/pending_dynamic_link_data.dart
@@ -25,14 +25,14 @@ class PendingDynamicLinkData {
   /// Deep link parameter of the dynamic link.
   final Uri link;
 
-  final Map<String, dynamic>? utmParameters;
+  final Map<dynamic, dynamic>? utmParameters;
 
   /// Returns the current instance as a [Map].
   Map<String, dynamic> asMap() => <String, dynamic>{
         'ios': ios?.asMap(),
         'android': android?.asMap(),
         'link': link.toString(),
-        'utmParameters': utmParameters ?? {},
+        'utmParameters': utmParameters,
       };
 
   @override
